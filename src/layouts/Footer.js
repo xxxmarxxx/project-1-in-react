fdc537bd2fec6cd25c296ad1893bc2bf9dcaeaae
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-30 17:13:23 +0100

    add image logo.svg

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "../styles/Footer.css";
+import image1 from "../assets/logo.svg"
 
 const Footer = () => {
+
+
   return (
     <>
       <footer class="footer page__footer">
-        <p class="footer_copyright">copyright</p> &nbsp;&bull;&nbsp;
-        <p class="footer__address-info">
-          {" "}
-          designArt &nbsp; 12164 berlin &nbsp; schlosstrasse 1000
+        <p class="footer_copyright">copyright</p>
+        <img src={image1} alt="image1" className="logoBottom__image" />
+        <p class="footer__address-info">{" "}
+       myArt &nbsp; 12164 berlin &nbsp; schlosstrasse 1000
         </p>
       </footer>
     </>
