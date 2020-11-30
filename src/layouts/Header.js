4c7b94d6ef4c407ca15ded9b2e8512525edbfc72
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-30 16:19:50 +0100

    add text  and className

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,22 +1,24 @@
 import React from "react";
 // import { Route, Switch } from 'react-router-dom'
-import image1 from "../assets/image_bg_header_1.jpg"
-import '../styles/Header.css';
+import image1 from "../assets/image_bg_header_1.jpg";
+import "../styles/Header.css";
 
 const Header = () => {
-
   return (
     <>
-    <div className="header">
-    <img src={image1} alt="image1" className="banner__image"/>
-    <h1>jestem header</h1>
-    </div>
-
-    
+      <div className="banner page__header">
+        <img src={image1} alt="image1" className="banner__image" />
+        <h1 class="banner__title banner__title--creative"> creative</h1>
+        <h2 class="banner__title banner__title--innovation"> innovation</h2>
+        <p class="banner__title banner__title--technology"> technology</p>
+        <p class="banner__title banner__title--success"> success</p>
+        <p class="banner__title banner__title--idea"> idea</p>
+        <p class="banner__title banner__title--development"> development</p>
+        <p class="banner__title banner__title--art"> art</p>
+        <button class="button banner__btn button--large">Wejdz</button>
+      </div>
     </>
-
   );
-  
-}
+};
 
 export default Header;
