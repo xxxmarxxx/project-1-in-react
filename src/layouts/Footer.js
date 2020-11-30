1f94b6612eca37ea0843aeaf03097a6f75da7ea7
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-30 14:37:24 +0100

    create Footer.js

---
+++ src/layouts/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+    return ( 
+
+<>
+<div className="footer">
+<p>jestem footer</p>
+
+</div>
+
+</>
+
+
+     );
+}
+ 
+export default Footer;
