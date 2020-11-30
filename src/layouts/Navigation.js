fb0a96409d298f127ba2a280d8f78da6f8758065
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-30 16:21:33 +0100

    create Navigation.js

---
+++ src/layouts/Navigation.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../styles/Navigation.css";
+import logo1 from "../assets/logo.svg";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="menu page__nav">
+        <div className="logo menu__logo">
+          <img src={logo1.svg} alt="" className="logo__image" />
+          <span className="logo__name">designArt</span>
+
+          <ul className="menu__list">
+            <li className="menu__item menu__item--is-open">
+              <a href="/" className="menu__link">
+                main
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="/" className="menu__link">
+                news
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="/" class="menu__link">
+                contact
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="/" className="menu__link">
+                impressum
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navigation;
