8fa91eaebe01568899675e0e899ac7e68c499cb7
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-11-30 14:37:05 +0100

    create App.js import Header.js, Footer.js App.css

---
+++ src/layouts/App.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header>{<Header />}</header>
+
+        <p>jajaj</p>
+      </div>
+      <footer>{<Footer />}</footer>
+    </>
+  );
+}
+
+export default App;
