4c667b55e77545912196f9e243dd843c23cd5a1e
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-12-08 22:58:36 +0100

    first commit create Cv.js

---
+++ src/layouts/Cv.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/Cv.css";
+import PageOne from "../componets/Cv_page_one";
+
+
+const Cv = () => {
+
+  return (
+    <>
+      <section className="aaaa">
+        jestem CV layout
+      </section>
+      <PageOne />
+    </>
+  );
+};
+
+export default Cv;
