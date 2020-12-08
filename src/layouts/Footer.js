5a44db049b1479ba95206c8dce87331f25f33119
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-12-08 22:59:00 +0100

    change adress

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         <p className="footer_copyright">copyright</p>
         <img src={image1} alt="image1" className="logoBottom__image" />
         <p className="footer__address-info">{" "}
-       myArt &nbsp; 12164 berlin &nbsp; schlosstrasse 1000
+       myArt &nbsp; 12164 berlin &nbsp; schlosstrasse 100
         </p>
       </footer>
     </>
