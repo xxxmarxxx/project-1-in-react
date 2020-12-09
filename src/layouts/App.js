e9c04dee8abded482851c861ca146dc6ae38afd5
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-12-09 13:13:50 +0100

    delete switch, route

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,9 +1,9 @@
+import React from "react";
 import Header from "./Header";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 import "../styles/App.css";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
@@ -11,11 +11,9 @@ function App() {
       <div className="App">
         <Header />
         <Navigation />
-        {/* <Page /> */}
         <Footer />
       </div>
     </Router>
   );
 }
-
 export default App;
