a6f8fc933f32ec61d12276d52f0731f9457acf78
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-12-16 16:46:35 +0100

    delete copyright

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -8,11 +8,14 @@ const Footer = () => {
   return (
     <>
       <footer className="footer page__footer">
-        <p className="footer_copyright">copyright</p>
+       
         <img src={image1} alt="image1" className="logoBottom__image" />
+        {/* <p className="footer_copyright">copyright</p> */}
         <p className="footer__address-info">{" "}
+        
        myArt &nbsp; 12164 berlin &nbsp; schlosstrasse 100
         </p>
+        
       </footer>
     </>
   );
