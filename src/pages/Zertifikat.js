869ba08260beb2e2ec2f13bb3f91b663b23b8a15
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-12-21 17:11:48 +0100

    create new page

---
+++ src/pages/Zertifikat.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactPDF from '@react-pdf/renderer';
+import MyDocument from '../pdf/Finales Zertifikat_FbW25-2.pdf';
+
+ReactPDF.render(<MyDocument />);
+
+// jeszcze nie zrobione
