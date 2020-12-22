0b3bd9a9903bcd175ff3004ea957adddf4241e0d
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-12-22 16:20:17 +0100

    import Document react-pdf

--- src/layouts/Cv.js
+++ src/layouts/Cv.js
@@ -2,14 +2,18 @@ import React from "react";
 import "../styles/Cv.css";
 import PageOne from "../componets/Cv_page_one";
 import PageSecond from "../componets/Cv_page_second";
-
+import { Document } from 'react-pdf';
 
 const Cv = () => {
   return (
     <>
       <PageOne />
       <PageSecond />
-     
+      <Document 
+      file="../pdf/Finales Zertifikat_FbW25-2.pdf"
+      
+      >
+        </Document>
     </>
   );
 };
