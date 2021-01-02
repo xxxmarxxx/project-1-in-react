f4868a3666d800a44390982bee91cc9c09f659d1
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-02 16:38:07 +0100

    update

--- src/layouts/Cv.js
+++ src/layouts/Cv.js
@@ -3,23 +3,21 @@ import "../styles/Cv.css";
 import PageOne from "../componets/Cv_page_one";
 import PageSecond from "../componets/Cv_page_second";
 // import { Document } from 'react-pdf';
-import PDF from '../componets/PDFReader'
-
-
+import PDF from "../componets/PDFReader";
 
 const Cv = () => {
   return (
     <>
-      <PageOne />
-      <PageSecond />
-      {/* <Document 
+      <div className="cv_wrapper">
+        <PageOne />
+        <PageSecond />
+        {/* <Document 
       file="../pdf/Finales Zertifikat_FbW25-2.pdf"
       
       >
         </Document> */}
         <PDF />
-        
-
+      </div>
     </>
   );
 };
