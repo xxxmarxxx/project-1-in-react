c75859b195c7cd9f794aa55f1b60352ce762aba3
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-07 19:45:36 +0100

    add more books

--- src/inputDate/inputBooks.js
+++ src/inputDate/inputBooks.js
@@ -19,6 +19,24 @@ export const BooksDate = [
       img: 'https://media.moluna.de/a449/514/9783954381241_2_EN_9783954381241_original_CBILD.JPG',
       title: 'Glückliche Ende',
       author: 'Issac Rosa'
+    },
+    {
+        id:4,
+      img: 'https://media.moluna.de/a2/677/9783453702981_2_EN_9783453702981_original_CBILD.JPG',
+      title: 'Was Tote über das Leben wissen',
+      author: 'Thomas John'
+    },
+    {
+        id:5,
+      img: 'https://media.moluna.de/a816/470/9783426282106_2_EN_9783426282106_original_CBILD.JPG',
+      title: 'Zwei Handvoll Leben',
+      author: 'Katharina Fuchs'
+    },
+    {
+        id:6,
+      img: 'https://media.moluna.de/a281/982/9783990391921_2_EN_9783990391921_original_CBILD.JPG',
+      title: 'Gleichgültigkeit',
+      author: 'Marica Bodrozic, Tanja Raich'
     }
 
   ];
