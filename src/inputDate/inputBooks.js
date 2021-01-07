aaf70cb7ce8641d1d4a36b6b5e6dd8b72e940b42
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-07 17:27:19 +0100

    create inputBooks.js

---
+++ src/inputDate/inputBooks.js
@@ -0,0 +1,24 @@
+
+
+export const BooksDate = [
+  
+    {
+       id:1,
+      img: 'https://media.moluna.de/a387/166/9783785727003_2_EN_9783785727003_original_CBILD.JPG',
+      title: 'Kingsbridge',
+      author: 'Ken Follett'
+   } ,
+    {
+        id:2,
+      img: 'https://media.moluna.de/a517/153/9783492070591_2_EN_9783492070591_original_CBILD.JPG',
+      title: 'Olympia',
+      author: 'Volker Kutscher'
+    },
+    {
+        id:3,
+      img: 'https://media.moluna.de/a449/514/9783954381241_2_EN_9783954381241_original_CBILD.JPG',
+      title: 'Glückliche Ende',
+      author: 'Issac Rosa'
+    }
+
+  ];
